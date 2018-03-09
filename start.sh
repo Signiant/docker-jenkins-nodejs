f89c14201b963c7a88e0fe7cc947939226bcc24b
Michael Perttula <mperttula@mikeP-mac.local>
2018-03-09 13:50:17 -0500

    Run build script instead of jenkins launch

--- start.sh
+++ start.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
-# Add any external credentials from a local data volume
-if [ -d "/credentials" ]; then
-	echo "Credentials folder found - processing any tar files"
-	for cred_file in /credentials/*.tar
-	do
-		echo "Processing credential file $cred_file"
-		tar xvpf $cred_file -C /home/$BUILD_USER
-	done
-fi
+echo "Running build script"
 
-# Just in case some of the tar files do not set correct owner
-#chown -R $BUILD_USER:$BUILD_USER_GROUP /home/$BUILD_USER
-
-if [ ! -z "$RUN_SLAVE" ]; then
-    echo "Downloading jenkins slave from $MASTER_ADDR"
-    wget -P / http://$MASTER_ADDR/jnlpJars/slave.jar
-    if [ -z "$SECRET" ]; then
-        su - $BUILD_USER -c "export HOME=/home/$BUILD_USER;\
-                             export JAVA_HOME=$JAVA_HOME;\
-                             export MAVEN_HOME=$MAVEN_HOME;\
-                             export OS=$SLAVE_OS;\
-          java -jar /slave.jar -jnlpUrl http://$MASTER_ADDR/computer/$SLAVE_ID/slave-agent.jnlp"
-    else
-        su - $BUILD_USER -c "export HOME=/home/$BUILD_USER;\
-                             export JAVA_HOME=$JAVA_HOME;\
-                             export MAVEN_HOME=$MAVEN_HOME;\
-                             export OS=$SLAVE_OS;\
-          java -jar /slave.jar -jnlpUrl http://$MASTER_ADDR/computer/$SLAVE_ID/slave-agent.jnlp -secret $SECRET"
-    fi
+node_version=$(grep "FROM node" app/Dockerfile | sed "s/.*node://g")
+re='^[0-9]+'
+if ! [[ $node_version =~ $re ]]; then
+	 # Node version is not a number - default to stable
+	 echo "Cannot determine node version from Docker file - defaulting to stable"
+	 node_version="stable"
 fi
+sudo n $node_version
+
+cd ./app
+#npm install --production
+#WHITESOURCE PLACEHOLDER
+echo "Installing all npm packages and running grunt tasks"
+npm install
+grunt
